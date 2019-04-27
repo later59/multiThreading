806397b9193451ad73c2dc4c746d059a2ac78ffb
DESKTOP-O99SV11\rock <later59@163.com>
2019-04-27 23:20:20 +0800

    初始化提交项目

---
+++ thread_project/src/main/java/com/rock/ThreadProjectApplication.java
@@ -0,0 +1,13 @@
+package com.rock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadProjectApplication.class, args);
+    }
+
+}
